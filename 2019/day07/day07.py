90d990da2bfbe52513ee262408a1d5001d3ad34a
Alexey Vasiliev <avasiliev@beamr.com>
2019-12-13 11:41:19 +0300

    day07 part2

--- 2019/day07/day07.py
+++ 2019/day07/day07.py
@@ -7,12 +7,14 @@ class Amplifier:
     def __init__(self, data, param):
         self.data = copy.copy(data)
         self.input_params = collections.deque([param])
- 
+        self.is_halted = False
+        self.instr_counter = 0
+
     def process(self, param):
         self.input_params.append(param)
         output_param = None
-        pc = 0
-        while self.data[pc] != 99:
+        pc = self.instr_counter
+        while self.is_halted == False:
             opcode = self.data[pc] % 100
             mode1 = self.data[pc] // 100 % 10
             mode2 = self.data[pc] // 1000 % 10
@@ -31,12 +33,17 @@ class Amplifier:
                 pc += 4
             elif opcode == 3:
                 res = self.data[pc+1]
-                self.data[res] = self.input_params.popleft()
-                pc += 2
+                try:
+                    self.data[res] = self.input_params.popleft()
+                    pc += 2
+                except IndexError:
+                    self.instr_counter = pc + 2
+                    break
             elif opcode == 4:
                 res = self.data[pc+1]
                 output_param = res if mode1 == 1 else self.data[res]
-                pc += 2
+                self.instr_counter = pc + 2
+                break
             elif opcode == 5:
                 param1 = self.data[pc+1]
                 param2 = self.data[pc+2]
@@ -75,13 +82,27 @@ class Amplifier:
                 else:
                     self.data[param3] = 0
                 pc += 4
+            elif opcode == 99:
+                self.is_halted = True
         return output_param
+
+
+def process2(data, pset):
+    amps = [ Amplifier(data, x) for x in pset ]    
+    param = 0
+    signal = 0
+    while True:
+        for i in range(0, 5):
+            param = amps[i].process(param)
+            if param is None:
+                return signal
+            signal = max(signal, param)
 
 
 def main():
     content = open('input07').read().rstrip('\n').split(',')    
     data = [ int(x) for x in content ]
-
+    
     psets = [ list(x) for x in itertools.permutations([0, 1, 2, 3, 4])]
     signal = 0
     for pset in psets:
@@ -92,6 +113,13 @@ def main():
         signal = max(signal, param)
     print(signal)
 
-
+    psets = [ list(x) for x in itertools.permutations([5, 6, 7, 8, 9])]
+    signal = 0
+    for pset in psets:
+        res = process2(data, pset)
+        signal = max(signal, res)
+    print(signal)
+    
+    
 if __name__ == '__main__':
     main()
