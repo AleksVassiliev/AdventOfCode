023660d54da1d47094f9c7ba5e8778d3abf891da
Alexey Vasiliev <avasiliev@beamr.com>
2019-12-17 11:22:09 +0300

    day09 part1

--- 2019/day09/day09.py
+++ 2019/day09/day09.py
@@ -33,15 +33,6 @@ class Computer:
     def add_input_param(self, value):
         self.input_params.append(value)
 
-    def get_value(self, addr, mode):
-        value = self.read(addr)
-        if mode == 0:
-            return self.read(value)
-        if mode == 1:
-            return value
-        if mode == 2:
-            return self.read(self.relative_base + value)
-
     def read(self, addr):
         try:
             return self.data[addr]
@@ -54,61 +45,89 @@ class Computer:
         except IndexError:
             self.external_memory[addr] = value
 
+    def get_value(self, addr, mode):
+        value = self.read(addr)
+        if mode == 0:
+            return self.read(value)
+        if mode == 1:
+            return value
+        if mode == 2:
+            return self.read(self.relative_base + value)
+
+    def get_addr(self, addr, mode):
+        value = self.read(addr)
+        if mode == 2:
+            return value + self.relative_base
+        else:
+            return value
+
     def opcode1(self, instr):
-        addr = self.data[self.instr_counter+3]
-        self.write(addr, self.get_value(self.instr_counter+1, instr.mode1) + self.get_value(self.instr_counter+2, instr.mode2))
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        param2 = self.get_value(self.instr_counter+2, instr.mode2)
+        param3 = self.get_addr(self.instr_counter+3, instr.mode3)
+        self.write(param3, param1 + param2)
         self.instr_counter += 4
 
     def opcode2(self, instr):
-        addr = self.data[self.instr_counter+3]
-        self.write(addr, self.get_value(self.instr_counter+1, instr.mode1) * self.get_value(self.instr_counter+2, instr.mode2))
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        param2 = self.get_value(self.instr_counter+2, instr.mode2)
+        param3 = self.get_addr(self.instr_counter+3, instr.mode3)
+        self.write(param3, param1 * param2)
         self.instr_counter += 4
 
     def opcode3(self, instr):
-        addr = self.data[self.instr_counter+1]
+        param1 = self.get_addr(self.instr_counter+1, instr.mode1)
         try:
-            self.data[addr] = self.input_params.popleft()
+            self.write(param1, self.input_params.popleft())
             self.instr_counter += 2
         except IndexError:
-            self.instr_counter += 2
             self.state = State.EWaitForInput
 
     def opcode4(self, instr):
-        value = self.get_value(self.instr_counter+1, instr.mode1)
-        self.output_params.append(value)
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        self.output_params.append(param1)
         self.instr_counter += 2
         self.state = State.ERaiseOutput
 
     def opcode5(self, instr):
-        if self.get_value(self.instr_counter+1, instr.mode1) != 0:
-            self.instr_counter = self.get_value(self.instr_counter+2, instr.mode2)
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        param2 = self.get_value(self.instr_counter+2, instr.mode2)
+        if param1 != 0:
+            self.instr_counter = param2
         else:
             self.instr_counter += 3
 
     def opcode6(self, instr):
-        if self.get_value(self.instr_counter+1, instr.mode1) == 0:
-            self.instr_counter = self.get_value(self.instr_counter+2, instr.mode2)
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        param2 = self.get_value(self.instr_counter+2, instr.mode2)
+        if param1 == 0:
+            self.instr_counter = param2
         else:
             self.instr_counter += 3
 
     def opcode7(self, instr):
-        addr = self.data[self.instr_counter+3]
-        if self.get_value(self.instr_counter+1, instr.mode1) < self.get_value(self.instr_counter+2, instr.mode2):
-            self.write(addr, 1)
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        param2 = self.get_value(self.instr_counter+2, instr.mode2)
+        param3 = self.get_addr(self.instr_counter+3, instr.mode3)
+        if param1 < param2:
+            self.write(param3, 1)
         else:
-            self.write(addr, 0)
+            self.write(param3, 0)
         self.instr_counter += 4
 
     def opcode8(self, instr):
-        addr = self.data[self.instr_counter+3]
-        if self.get_value(self.instr_counter+1, instr.mode1) == self.get_value(self.instr_counter+2, instr.mode2):
-            self.write(addr, 1)
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        param2 = self.get_value(self.instr_counter+2, instr.mode2)
+        param3 = self.get_addr(self.instr_counter+3, instr.mode3)
+        if param1 == param2:
+            self.write(param3, 1)
         else:
-            self.write(addr, 0)
+            self.write(param3, 0)
         self.instr_counter += 4
 
     def opcode9(self, instr):
-        self.relative_base += self.data[self.instr_counter+1]
+        param1 = self.get_value(self.instr_counter+1, instr.mode1)
+        self.relative_base += param1
         self.instr_counter += 2
 
     def opcode99(self, instr):
@@ -139,18 +158,19 @@ class Computer:
             self.opcode99(instr)
 
 
-def process(data):
+def process1(data):
     c = Computer(data)
+    c.add_input_param(1 )
     while c.state != State.EHalted:
         c.process()
-    print(c.output_params)
+    print(c.output_params[0])
 
 
 def main():
-    content = open('input09').read().rstrip('\n').split(',')    
+    content = open('input09').read().rstrip('\n').split(',')
     data = [ int(x) for x in content ]
 
-    process(data)
+    process1(data)
     
     
 if __name__ == '__main__':
