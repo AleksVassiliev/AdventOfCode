edcc6ba6a19ac5c3dc298e8e26a79fae1315097c
Alexey Vasiliev <avasiliev@beamr.com>
2019-12-18 11:44:14 +0300

    day10 part2

--- 2019/day10/day10.py
+++ 2019/day10/day10.py
@@ -1,4 +1,6 @@
 import math
+import collections
+
 
 def count_asteroids(data, x0, y0):
     angles = set()
@@ -12,16 +14,49 @@ def count_asteroids(data, x0, y0):
     return len(angles)
 
 
+def destroy_asteroids(data, x0, y0):
+    asteroids = collections.defaultdict(list)
+    for y, line in enumerate(data):
+        for x, cell in enumerate(line):
+            if cell == '#':
+                angle = math.atan2(x0-x, y0-y) * 180 / math.pi
+                if angle <= 0:
+                    angle += 360
+                distance = math.sqrt(pow(x0-x, 2) + pow(y0-y, 2))
+                if (x0 != x) or (y0 != y):
+                    asteroids[angle].append((x, y, distance))
+    for k in asteroids:
+        asteroids[k].sort(key=lambda x: x[2])
+    
+    angles = list(asteroids.keys())
+    angles.sort(reverse=True)
+    idx = 0
+    while True:
+        for k in angles:
+            try:
+                a = asteroids[k].pop(0)
+                idx += 1
+                if idx == 200:
+                    return (a[0] * 100 + a[1])
+            except IndexError:
+                pass
+
+
 def main():
     data = [ x.rstrip('\n') for x in open('input10') ]
 
     result = 0
+    point = (0, 0)
     for j, line in enumerate(data):
         for i, cell in enumerate(line):
             if cell == '#':
                 res = count_asteroids(data, i, j)
-                result = max(res, result)
+                if res > result:
+                    result = res
+                    point = (i, j)
     print(result)
+
+    print(destroy_asteroids(data, point[0], point[1]))
 
     
 if __name__ == '__main__':
