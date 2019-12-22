c5ff07c274ccc1b9745b08384f8a08a8110f9963
Aleksei Vassiliev <aleksvassiliev@gmail.com>
2019-12-22 11:49:42 +0300

    day12 part2

--- 2019/day12/day12.py
+++ 2019/day12/day12.py
@@ -1,4 +1,22 @@
+import math
 import itertools
+
+
+class utils:
+    @staticmethod
+    def period(seq):
+        l = len(seq) // 2
+        for i in range(l):
+            if seq[i] != seq[i+l]:
+                return 0
+        return l
+
+    @staticmethod
+    def gcd(seq):
+        a = seq[0]
+        for b in seq[1:]:
+            a = (a * b) // math.gcd(a, b)
+        return a
 
 
 class Vector:
@@ -17,6 +35,15 @@ class Vector:
         self.z += other.z
         return self
 
+    def __eq__(self, other):
+        if self.x != other.x:
+            return False
+        if self.y != other.y:
+            return False
+        if self.z != other.z:
+            return False
+        return True
+
     def value(self):
         return abs(self.x) + abs(self.y) + abs(self.z)
 
@@ -25,6 +52,8 @@ class Moon:
     def __init__(self, x, y, z):
         self.position = Vector(x, y, z)
         self.velocity = Vector(0, 0, 0)
+        self.history = [ [ x ], [ y ], [ z ] ]
+        self.period = [0, 0, 0]
 
     def __str__(self):
         repr_str = 'position={}, velocity={}'.format(self.position, self.velocity)
@@ -35,9 +64,26 @@ class Moon:
 
     def change_position(self):
         self.position += self.velocity
+        if self.period[0] == 0:
+            self.history[0].append(self.position.x)
+            self.period[0] = utils.period(self.history[0])
+        if self.period[1] == 0:
+            self.history[1].append(self.position.y)
+            self.period[1] = utils.period(self.history[1])
+        if self.period[2] == 0:
+            self.history[2].append(self.position.z)
+            self.period[2] = utils.period(self.history[2])
 
     def energy(self):
         return self.position.value() * self.velocity.value()
+
+    def is_initial(self):
+        if 0 in self.period:
+            return False
+        return True
+
+    def mperiod(self):
+        return utils.gcd(self.period)
 
 
 class MoonSystem:
@@ -65,7 +111,7 @@ class MoonSystem:
             v0, v1 = self.calculate_velocity(c[0], c[1])
             c[0].change_velocity(v0)
             c[1].change_velocity(v1)
-        for m in self.moons:
+        for idx, m in enumerate(self.moons):
             m.change_position()
 
     def velocity(self, value1, value2):
@@ -88,14 +134,38 @@ class MoonSystem:
             res += m.energy()
         return res
 
+    def is_initial(self):
+        res = True
+        for m in self.moons:
+            res = res and m.is_initial()
+        return res
 
-def main():
-    data = [ x.rstrip('\n') for x in open('input12') ]
+    def period(self):
+        p = []
+        for m in self.moons:
+            p.append(m.mperiod())
+        return utils.gcd(p)
 
+
+def part1(data):
     s = MoonSystem(data)
     for i in range(1000):
         s.simulate()
-    print(s.energy())
+    return s.energy()
+
+
+def part2(data):
+    s = MoonSystem(data)
+    while s.is_initial() == False:
+        s.simulate()
+    return s.period()
+
+
+def main():
+    data = [ x.rstrip('\n') for x in open('input12') ]
+    print(part1(data))
+    print(part2(data))
+
 
 if __name__ == '__main__':
     main()
