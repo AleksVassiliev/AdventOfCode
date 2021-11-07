eac95ebff37cf285bfe33b9ce633b2d62934615b
Aleksei Vassiliev <aleksvassiliev@gmail.com>
2021-11-07 09:42:25 +0300

    day13 part2

--- 2020/day13/day13.py
+++ 2020/day13/day13.py
@@ -1,3 +1,6 @@
+from functools import reduce
+
+
 def calculate_delay(timestamp, schedule):
     ids = [int(x) for x in schedule.split(',') if x != 'x']
     result = []
@@ -11,13 +14,40 @@ def calculate_delay(timestamp, schedule):
     return (result[0][0] - timestamp) * result[0][1]
 
 
+def mul_inv(a, b):
+    b0 = b
+    x0, x1 = 0, 1
+    if b == 1:
+        return 1
+    while a > 1:
+        q = a // b
+        a, b = b, a % b
+        x0, x1 = x1 - q * x0, x0
+    if x1 < 0:
+        x1 += b0
+    return x1
+
+
+def chinese_remainder(n, a):
+    sum = 0
+    prod = reduce(lambda x, y: x * y, n)
+    for n_i, a_i in zip(n, a):
+        p = prod // n_i
+        sum += a_i * mul_inv(p, n_i) * p
+    return sum % prod
+
+
 def calculate_timestamp(schedule):
-    pass
+    schedule = schedule.split(',')
+    buses = [int(b) for b in schedule if b != 'x']
+    offsets = [int(b) - i for i, b in enumerate(schedule) if b != 'x']
+    return chinese_remainder(buses, offsets)
 
 
 def main():
-    timestamp = 1005162
-    schedule = '19,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,x,x,x,823,x,x,x,x,x,x,x,23,x,x,x,x,x,x,x,x,17,x,x,x,x,x,x,x,x,x,x,x,29,x,443,x,x,x,x,x,37,x,x,x,x,x,x,13'
+    with open('input13.txt') as fi:
+        timestamp = int(fi.readline())
+        schedule = fi.readline()
     result_v1 = calculate_delay(timestamp, schedule)
     print(result_v1)
     result_v2 = calculate_timestamp(schedule)
--- 2020/day13/test_day13.py
+++ 2020/day13/test_day13.py
@@ -8,7 +8,7 @@ def test_schedule_v1(timestamp, schedule, result):
     assert(day13.calculate_delay(timestamp, schedule) == result)
 
 
-@pytest.mark.parametrize('schedule, result', [('7,13,x,x,59,x,31,19', 295), ('17,x,13,19', 3417), ('67,7,59,61', 754018), ('67,x,7,59,61', 779210),
+@pytest.mark.parametrize('schedule, result', [('17,x,13,19', 3417), ('67,7,59,61', 754018), ('67,x,7,59,61', 779210),
                                               ('67,7,x,59,61', 1261476), ('1789,37,47,1889', 1202161486)])
 def test_schedule_v2(schedule, result):
     assert(day13.calculate_timestamp(schedule) == result)
